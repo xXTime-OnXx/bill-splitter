b39018a4d7b575156ae0500a9b1217ba15093f88
xXTime-OnXx <timon.schmid@bluewin.ch>
2021-11-10 21:39:54 +0100

    swagger open api functionality

--- bill-splitter-backend/src/controller/rest/auth/auth.controller.ts
+++ bill-splitter-backend/src/controller/rest/auth/auth.controller.ts
@@ -2,17 +2,21 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../../security/auth/guard/local-auth.guard';
 import { AuthService } from '../../security/auth/auth.service';
 import { JwtAuthGuard } from '../../security/auth/guard/jwt-auth.guard';
+import { ApiBasicAuth, ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiBasicAuth()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req): Promise<any> {
     return this.authService.login(req.user);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req): Promise<void> {
