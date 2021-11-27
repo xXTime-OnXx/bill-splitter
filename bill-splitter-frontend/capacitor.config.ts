9207bd5ad4547de940c21181c77bc0d68555ff8d
xXTime-OnXx <timon.schmid@bluewin.ch>
2021-11-27 15:27:27 +0100

    added register page

---
+++ bill-splitter-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appName: 'bill-splitter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
