a8d3d21a06a2ed3694bf0ac8a5a9d86bf503c6a6
xXTime-OnXx <timon.schmid@bluewin.ch>
2022-01-23 17:24:42 +0100

    resolved warnings and incompatibilities

--- bill-splitter-frontend/src/app/service/auth/auth.interceptor.ts
+++ bill-splitter-frontend/src/app/service/auth/auth.interceptor.ts
@@ -1,11 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Router} from '@angular/router';
-import {from, Observable} from 'rxjs';
-import {map, tap} from 'rxjs/operators';
+import {from, mergeMap, Observable, tap} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {StorageService} from '../storage/storage.service';
 import {environment} from '../../../environments/environment';
-import {flatMap} from 'rxjs/internal/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -24,13 +23,14 @@ export class AuthInterceptor implements HttpInterceptor {
           }
           return request;
         }),
-        flatMap((clonedRequest) => next.handle(clonedRequest).pipe(tap(() => {
-          }, (err) => {
+        mergeMap((clonedRequest) => next.handle(clonedRequest).pipe(tap({
+          error: (err) => {
             if (err instanceof HttpErrorResponse && err.status === 401) {
               this.router.navigate(['login']);
             }
             return;
-          })))
+          }
+        })))
       );
   }
 }
