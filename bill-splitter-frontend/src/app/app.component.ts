8a1a93fde7dd885bac68faed4f22f6817d89b336
xXTime-OnXx <timon.schmid@bluewin.ch>
2022-01-25 20:11:32 +0100

    Status Bar Plugin

--- bill-splitter-frontend/src/app/app.component.ts
+++ bill-splitter-frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StatusBar, Style} from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor() {
+  }
+
+  async ngOnInit(): Promise<void> {
+    await StatusBar.setStyle({style: Style.Dark})
+  }
 }
