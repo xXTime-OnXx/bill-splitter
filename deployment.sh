926f184e300922f0e7c029eef8ec3c400800e5ba
xXTime-OnXx <timon.schmid@bluewin.ch>
2022-01-30 13:59:49 +0100

    fixed deployment script

--- deployment.sh
+++ deployment.sh
@@ -3,5 +3,5 @@
 docker-compose up -d
 
 cd bill-splitter-backend
-docker build -t bill-splitter/bill-splitter-backend
+docker build -t bill-splitter/bill-splitter-backend .
 docker-compose up --recreate -d
